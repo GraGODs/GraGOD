e07dc1ecedfc7953ae23b7329861a0371d80bdc0
Federico Bello <fe.debello13@gmail.com>
2024-11-07 18:04:40 -0300

    feat: add a general dataset

---
+++ datasets/dataset.py
@@ -0,0 +1,63 @@
+import torch
+from torch.utils.data import Dataset
+
+
+class SlidingWindowDataset(Dataset):
+    def __init__(
+        self,
+        data: torch.Tensor,
+        edge_index: torch.Tensor,
+        window_size: int,
+        horizon: int = 1,
+        labels: torch.Tensor | None = None,
+    ):
+        self.data = data
+        self.labels = labels
+        self.edge_index = edge_index
+        self.window_size = window_size
+        self.horizon = horizon
+
+        self.valid_indices = self._get_valid_indices()
+
+    def __len__(self):
+        return len(self.valid_indices)
+
+    def __getitem__(self, index):
+        if index >= len(self.valid_indices):
+            raise IndexError("Index out of range")
+
+        valid_idx = self.valid_indices[index]
+
+        x = self.data[valid_idx : valid_idx + self.window_size]
+        y = self.data[
+            valid_idx + self.window_size : valid_idx + self.window_size + self.horizon
+        ]
+
+        out_labels = (
+            self.labels[
+                valid_idx
+                + self.window_size : valid_idx
+                + self.window_size
+                + self.horizon
+            ]
+            if self.labels is not None
+            else None
+        )
+
+        return x, y, out_labels, self.edge_index
+
+    def _get_valid_indices(self):
+        if self.labels is None:
+            # if there are no labels, all indices are valid
+            return torch.arange(len(self.data) - self.window_size)
+
+        # an index is valid if all the labels between
+        # [index, index + window_size + horizon] are 0
+        valid_indices_mask = [
+            any(self.labels[i : i + self.window_size + self.horizon])
+            for i in range(len(self.data) - self.window_size - self.horizon)
+        ]
+
+        valid_indices = torch.where(torch.tensor(valid_indices_mask))[0]
+
+        return valid_indices
