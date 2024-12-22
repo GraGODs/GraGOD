645566bd92fcf6fd8025b9d6c8bc3e4db82c6a79
gonzachiar <gonzalochiarlone@gmail.com>
2024-12-22 17:15:56 +0000

    add show metrics command

--- gragod/cli.py
+++ gragod/cli.py
@@ -1,4 +1,8 @@
+import json
+
 import typer
+
+from gragod.metrics import generate_metrics_per_class_table, generate_metrics_table
 
 app = typer.Typer()
 
@@ -11,6 +15,15 @@ def test_cli():
 @app.command()
 def hello(name: str):
     typer.echo(f"Hello {name}")
+
+
+@app.command()
+def show_metrics(metric_path: str, per_class: bool = False):
+    with open(metric_path, "r") as f:
+        metric = json.load(f)
+    typer.echo(generate_metrics_table(metric))
+    if per_class:
+        typer.echo(generate_metrics_per_class_table(metric))
 
 
 if __name__ == "__main__":
