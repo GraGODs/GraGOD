176eb77ca0903bce86367899e529ad86998efdd6
gonzachiar <gonzalochiarlone@gmail.com>
2024-12-31 16:24:57 -0300

    small fix in load_checkpoint_path

--- gragod/utils.py
+++ gragod/utils.py
@@ -4,6 +4,7 @@ import sys
 from pathlib import Path
 
 import torch
+from colorama import Fore
 
 from gragod.types import PathType
 
@@ -22,25 +23,44 @@ def get_logger(logger_name: str | None = None):
     return logger
 
 
-def load_checkpoint_path(checkpoint_folder: PathType, experiment_name: str) -> Path:
+def load_checkpoint_path(checkpoint_folder: str, experiment_name: str) -> Path:
+    """
+    Load the checkpoint path from the checkpoint folder.
+    If the checkpoint folder ends with ".ckpt", it is used as the checkpoint path.
+    Otherwise, the checkpoint path is the best.ckpt file in the checkpoint folder.
+
+    If the checkpoint path does not exist, it tries to load the checkpoint from the
+    checkpoint folder with the experiment name.
+
+    Args:
+        checkpoint_folder: The folder containing the checkpoint.
+        experiment_name: The name of the experiment.
+
+    Returns:
+        The checkpoint path.
+    """
     checkpoint_path = os.path.join(checkpoint_folder, "best.ckpt")
 
     if not os.path.exists(checkpoint_path):
-        print(
-            f"Checkpoint not found at {checkpoint_path}. Trying with {experiment_name}"
-        )
-        checkpoint_path = os.path.join(
+        checkpoint_path_candidate = os.path.join(
             checkpoint_folder,
             f"{experiment_name}.ckpt",
         )
-        if not os.path.exists(checkpoint_path):
-            raise ValueError(
-                f"Checkpoint not found at {checkpoint_path}. "
-                f"Tried with {experiment_name}"
-            )
+        print(
+            Fore.YELLOW + f"Checkpoint not found at {checkpoint_path}. "
+            f"Trying with {checkpoint_path_candidate}" + Fore.RESET
+        )
 
-    if not checkpoint_path.endswith(".ckpt"):
-        raise ValueError(f"Checkpoint must end with .ckpt, got {checkpoint_path}")
+        if not os.path.exists(checkpoint_path_candidate):
+            print(
+                Fore.YELLOW
+                + f"Tried with {checkpoint_path_candidate}, but it does not exist."
+                + Fore.RESET
+            )
+            raise ValueError(
+                f"Checkpoint not found at {checkpoint_path} or"
+                f"{checkpoint_path_candidate}"
+            )
 
     return Path(checkpoint_path)
 
