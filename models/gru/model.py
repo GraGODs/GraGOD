0181fee1cd9f6d54544808af61bfedf8b9d070bb
gonzachiar <gonzalochiarlone@gmail.com>
2025-01-02 09:38:24 -0300

    fix gru for predicting

--- models/gru/model.py
+++ models/gru/model.py
@@ -136,3 +136,13 @@ class GRU_PLModule(PLBaseModule):
         x = batch[0] if isinstance(batch, (list, tuple)) else batch
         predictions = self(x)
         return predictions
+
+    def post_process_predictions(self, predictions):
+        """Post-process the predictions."""
+        predictions = torch.cat(predictions)
+        return predictions
+
+    def calculate_anomaly_score(self, predict_output, X_true, **kwargs):
+        """Calculate the anomaly score."""
+        predictions = self.post_process_predictions(predict_output)
+        return torch.abs(predictions - X_true)
