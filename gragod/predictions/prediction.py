0590893ffc1422f2a4f326b633466284b1d8b5bb
Federico Bello <fbello@tryolabs.com>
2025-02-14 16:55:38 +0000

    feat: range based f1 for system labels threshold

--- gragod/predictions/prediction.py
+++ gragod/predictions/prediction.py
@@ -16,7 +16,7 @@ def get_threshold(
     range_based: bool = True,
 ) -> torch.Tensor:
     if labels.ndim == 0 or labels.shape[1] in [0, 1]:
-        return get_threshold_system(dataset, scores, labels, n_thresholds)
+        return get_threshold_system(dataset, scores, labels, n_thresholds, range_based)
     else:
         return get_threshold_per_class(
             dataset, scores, labels, n_thresholds, range_based
@@ -98,7 +98,11 @@ def get_threshold_per_class(
 
 
 def get_threshold_system(
-    dataset: Datasets, scores: torch.Tensor, labels: torch.Tensor, n_thresholds: int
+    dataset: Datasets,
+    scores: torch.Tensor,
+    labels: torch.Tensor,
+    n_thresholds: int,
+    range_based: bool = True,
 ) -> torch.Tensor:
     """
     Get the threshold for the scores.
@@ -118,9 +122,14 @@ def get_threshold_system(
     metrics = MetricsCalculator(
         dataset=dataset, labels=labels, predictions=preds, scores=scores
     )
-    precision = metrics.calculate_precision()
-    recall = metrics.calculate_recall()
-    f1 = metrics.calculate_f1(precision, recall)
+    if range_based:
+        precision = metrics.calculate_range_based_precision()
+        recall = metrics.calculate_range_based_recall()
+        f1 = metrics.calculate_range_based_f1(precision, recall)
+    else:
+        precision = metrics.calculate_precision()
+        recall = metrics.calculate_recall()
+        f1 = metrics.calculate_f1(precision, recall)
 
     system_f1 = f1.metric_system
 
@@ -132,9 +141,15 @@ def get_threshold_system(
         metrics = MetricsCalculator(
             dataset=dataset, labels=labels, predictions=preds, scores=scores
         )
-        precision = metrics.calculate_precision()
-        recall = metrics.calculate_recall()
-        f1 = metrics.calculate_f1(precision, recall)
+        if range_based:
+            precision = metrics.calculate_range_based_precision()
+            recall = metrics.calculate_range_based_recall()
+            f1 = metrics.calculate_range_based_f1(precision, recall)
+        else:
+            precision = metrics.calculate_precision()
+            recall = metrics.calculate_recall()
+            f1 = metrics.calculate_f1(precision, recall)
+
         # Update best thresholds where F1
         if f1.metric_system > system_f1:
             system_f1 = f1.metric_system
