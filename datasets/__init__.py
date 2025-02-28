3869834e2d3bf38dac14b05f8c34523f480673f5
Federico Bello <fbello@tryolabs.com>
2025-02-28 22:45:19 +0000

    ute config dataset

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -1,3 +1,4 @@
 from .mihaela import load_mihaela_service_training_data
 from .swat import load_swat_training_data
 from .telco import load_telco_training_data
+from .ute import load_ute_training_data
