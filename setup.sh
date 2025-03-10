7b05b94140c4663b3abc23ce75688ca7ebdab763
gonzachiar <gonzalochiarlone@gmail.com>
2025-03-10 23:44:09 +0000

    add bash files

---
+++ setup.sh
@@ -0,0 +1,9 @@
+apt update
+apt install tmux -y
+apt install vim -y
+apt install htop -y
+
+git config --global user.email "gonzalochiarlone@gmail.com"
+git config --global user.name "gonzachiar"
+
+source .venv/bin/activate
