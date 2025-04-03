4714a131cd3df075b2d44828cf3f85fb2eae9c3d
Federico Bello <fbello@tryolabs.com>
2025-04-03 22:44:30 +0000

    fix: squeeze in system calculator

--- gragod/metrics/system_calculator.py
+++ gragod/metrics/system_calculator.py
@@ -33,8 +33,14 @@ class SystemCalculator(MetricsCalculator[SystemMetricsResult]):
         """
         super().__init__(dataset)
         self.system_labels = system_labels
+        if self.system_labels.ndim == 2:
+            self.system_labels = self.system_labels.squeeze()
         self.system_predictions = system_predictions
+        if self.system_predictions.ndim == 2:
+            self.system_predictions = self.system_predictions.squeeze()
         self.system_scores = system_scores
+        if self.system_scores.ndim == 2:
+            self.system_scores = self.system_scores.squeeze()
 
     def calculate_precision(self) -> SystemMetricsResult:
         """
